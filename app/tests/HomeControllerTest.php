6abbc787f97688b45db46241d14befd90d3613fa
Geoffrey BERGERET <geoffrey.bergeret@gbergeret.org>
2015-01-25 16:37:00 +0100

    Update redirect route test.

--- app/tests/HomeControllerTest.php
+++ app/tests/HomeControllerTest.php
@@ -2,16 +2,22 @@
 
 class HomeControllerTest extends TestCase {
 
+	public function setUp() {
+		parent::setUp(); 
+ 
+		$this->client->setServerParameter('HTTP_ACCEPT_LANGUAGE', 'en');
+	}
+	
 	public function testIndex() {
 		
 		$this->client->request('GET', '/');
-		$this->assertRedirectedToRoute('home.index', [], ['lang']);
+		$this->assertRedirectedToRoute('home.index', ['lang' => 'en']);
 		
 	}
 	public function testIndexInEn() {
 		
 		$this->client->request('GET', '/en');
-		$this->assertResponseOk();;
+		$this->assertResponseOk();
 		
 	}
 	public function testIndexInFr() {
