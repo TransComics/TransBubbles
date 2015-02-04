644f3e506dfa7f0f4a344680c9621fd0c91cfb16
ILovePanda <antoine.masline.camerini@gmail.com>
2015-02-04 01:08:30 +0100

    Split clean and translation view

--- app/views/layouts/tool.blade.php
+++ app/views/layouts/tool.blade.php
@@ -15,28 +15,16 @@
 <div id='top-line'>
 </div>
 <header id='header'>
-		<h1>Trans<span class="cyan-text">Bubbles</span> - Interface d'import</h1>
+		<h1>Trans<span class="cyan-text">Bubbles</span> - @yield('tool.title')</h1>
 </header>
 <nav id='nav'>
 	<ul id="origin-remote">
 		<li><a class='button' href="" id="hidden-origin">Cacher</a></li>
 	</ul>
 	<ul id="delivered-remote">
-		<li><a class='icon icon-update zero-padding' href="" id="update"></a></li>
-		<li><a class='icon icon-text zero-padding' href="" id="text"></a></li>
-		<li><a class='icon icon-del zero-padding' href="" id="del"></a></li>
-		<li><a class='icon icon-brush zero-padding' href="" id="brush"></a></li>
-		<li><a class='icon icon-rect zero-padding' href="" id="rect"></a></li>
-		<li><a class='icon icon-circle zero-padding' href="" id="circle"></a></li>
-		<li><a class='icon icon-viewAll zero-padding' href="" id="viewAll"></a></li>
-		<li><span class='icon icon-selectAll zero-padding' href="" id="selectAll"></span></li>
 		
-		<li><a class='icon icon-zoomm zero-padding' href="" id="btnZoomOut"></a></li>
-		<li><a class='icon icon-zoomp zero-padding' href="" id="btnZoomIn"></a></li>
-		<li><a class='icon icon-zoom zero-padding' href="" id="btnResetZoom"></a></li>
-			
-		<li><a class='icon icon-undo zero-padding' href="" id="undo"></a></li>
-		<li><a class='icon icon-redo zero-padding' href="" id="redo"></a></li>
+		@yield('tool.items')
+		
 	</ul>
 </nav>
 
