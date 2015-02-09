18376e6b710e9d57965f65d3e77c247ab9a6d770
Guillaume Roux <gu.roux@gmail.com>
2015-02-09 09:16:32 +0100

    Adding strips english translations

---
+++ app/lang/en/strips.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+    'send' => 'Send',
+    'title' => 'Title',
+    'author' => 'Author',
+    'pageNumber' => 'Page Number',
+    'send' => 'Send',
+    'uploadComplete' => 'Your image has been transferred',
+    'uploadFailed' => 'Your image have not been transfered'
+);
