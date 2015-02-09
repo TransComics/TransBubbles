ed2f16231cae10d73dcef54ec7c005688dc88d77
amasline <amasline@172.22.247.110>
2015-02-09 10:33:33 +0100

    add seed data for comics

--- app/database/seeds/ComicsTableSeeder.php
+++ app/database/seeds/ComicsTableSeeder.php
@@ -5,16 +5,18 @@ class ComicsTableSeeder extends Seeder {
     public function run () {
         DB::table('comics')->insert([
             [
-                'title' => 'Spiderman I',
-                'author' => 'gbergere',
-                'description' => 'Description SI',
-                'authorApproval' => 1,
+                'title' => 'Strumph',
+                'author' => 'AMC',
+                'description' => 'Description Strumph',
+                'authorApproval' => true,
+                'cover' => '/images/comics/0/ce7afa9115bb0b891963c632be0c4696',
             ],
             [
                 'title' => 'Spiderman II',
                 'author' => 'gbergereI',
                 'description' => 'Description SII',
-                'authorApproval' => 1,
+                'authorApproval' => true,
+                'cover' => '/images/comics/0/d9480b185525aa7711522b34544fb0f6',
             ]
         ]);
     }
