ba90b189765ac939c3fc3cd39e73ca89691f7a4a
Guillaume Roux <gu.roux@gmail.com>
2015-02-09 10:18:46 +0100

    Updating Strips model

--- app/models/Strips.php
+++ app/models/Strips.php
@@ -2,4 +2,12 @@
 
 class Strips extends Eloquent {
 
+    protected $table = 'strips';
+    public $timestamps = true;
+    protected $guarded = array('id', 'updated_at', 'created_at',
+        'insertion_date', 'path');
+
+    public static $rules = [
+        'pageNumber' => 'numeric'
+    ];
 }
