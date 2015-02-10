6dcc12206fa76295592224e3f6675e7a2073f5d0
Guillaume Roux <gu.roux@gmail.com>
2015-02-10 16:03:13 +0100

    Fixing home links for Strips

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -47,7 +47,7 @@
                         <a href="{{URL::route('user.signin')}}" class="list-group-item" >@lang('user.signIn')</a>
                     @endif
                     <a href="{{URL::route('comics.list')}}" class="list-group-item" >@lang('comics.listLink')</a>
-                    <a href="{{URL::route('strip.import', ['id' => 3])}}" class="list-group-item" >@lang('strip.importLink')</a>
+                    <a href="{{URL::route('strips.index')}}" class="list-group-item" >@lang('strip.importLink')</a>
                     <a href="{{URL::route('strip.clean', ['id' => 3])}}" class="list-group-item" >@lang('strip.cleanLink')</a>
                     <a href="{{URL::route('strip.translate', ['id' => 3])}}" class="list-group-item" >@lang('strip.translateLink')</a>
                 </div>
