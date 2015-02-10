696deb4628daf3bb75ca4ebded503b0dd6e6a0d1
Guillaume Roux <gu.roux@gmail.com>
2015-02-10 23:50:06 +0100

    Updating validation rules

--- app/models/Strips.php
+++ app/models/Strips.php
@@ -5,12 +5,15 @@ class Strips extends Eloquent {
     protected $table = 'strips';
     public $timestamps = true;
     protected $guarded = [ 'id', 'updated_at', 'created_at',
-        'insertion_date', 'path'];
+        'insertion_date', 'path', 'validated_at'];
     public static $rules = [
         'pageNumber' => 'numeric',
+        'title' => 'max:64',
         'strip' => 'required|mimes:jpeg,bmp,png,tiff,tif,jpg|max:4096|image'
     ];
     
-    public static $updateRules = [];
+    public static $updateRules = [
+        'title' => 'max:64'
+    ];
 
 }
