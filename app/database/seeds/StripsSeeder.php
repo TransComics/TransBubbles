95a2d78e8f2e83f0d958946653de35f4952da096
Guillaume Roux <gu.roux@gmail.com>
2015-02-10 11:31:39 +0100

    Updating seeder

--- app/database/seeds/StripsSeeder.php
+++ app/database/seeds/StripsSeeder.php
@@ -1,19 +1,31 @@
 <?php
 
-class StripSeeder extends Seeder {
+class StripsSeeder extends Seeder {
 
-    public function run () {
+    public function run() {
         DB::table('strips')->insert([
             [
-                'title' => 'Tintin',
-                'path' => '/usr/books/tintin/page1.png',
-                'pageNumber' => 1,
+                'id' => 1,
+                'title' => 'Je suis projet',
+                'path' => '/uploads/0/363b1c29fe79ecbd4a1777201ce11dee',
+                'created_at' => '2015-02-10 10:49:43',
+                'updated_at' => '2015-02-10 10:49:43'
             ],
             [
-                'title' => 'Tintin',
-                'path' => '/usr/books/tintin/page2.png',
-                'pageNumber' => 2,
+                'id' => 2,
+                'title' => 'Portable du père',
+                'path' => '/uploads/0/cdb8a777ccf2c7c539cafe4e670c72e2',
+                'created_at' => '2015-02-10 10:50:01',
+                'updated_at' => '2015-02-10 10:50:01'
+            ],
+            [
+                'id' => 3,
+                'title' => 'Devop ?',
+                'path' => '/uploads/0/58f19710798ee712be8efaeba131f768',
+                'created_at' => '2015-02-10 10:50:09',
+                'updated_at' => '2015-02-10 10:50:09'
             ]
         ]);
     }
+
 }
