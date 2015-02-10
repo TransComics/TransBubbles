72c3a4dcdf4292fa191f5b05a3abf808215958e2
Guillaume Roux <gu.roux@gmail.com>
2015-02-10 10:00:59 +0100

    Updating messages

--- app/lang/en/strips.php
+++ app/lang/en/strips.php
@@ -7,5 +7,12 @@ return array(
     'pageNumber' => 'Page Number',
     'send' => 'Send',
     'uploadComplete' => 'Your image has been transferred',
-    'uploadFailed' => 'Your image have not been transfered'
+    'uploadFailed' => 'Your image have not been transfered',
+    'addTitle' => 'Add a Strip',
+    'stripFileSelector' => 'Choose the strip to share',
+    'unvalidFile' => 'Unvalid file',
+    'updateFailure' => 'Failed to update the Strip',
+    'editComplete' => 'Editing completed',
+    //CORRIGER !!!
+    'deleteSucceded' => 'The strip have been deleted'
 );
