96cc1da31b5ce8ddc9f2428d003c3dd47f06f4d6
Geoffrey BERGERET <geoffrey.bergeret@gbergeret.org>
2015-02-10 10:39:06 +0100

    Add created_by field in Comics with management in Update and Create form.

--- app/database/migrations/2015_02_06_100748_create_comics.php
+++ app/database/migrations/2015_02_06_100748_create_comics.php
@@ -19,11 +19,19 @@ class CreateComics extends Migration {
             $table->boolean('authorApproval');
             $table->string('cover',127)->nullable();
             $table->integer('font_id')->unsigned()->nullable();
+            $table->integer('created_by')->unsigned()->nullable();
             $table->timestamps();
         });
         
         Schema::table('comics', function(Blueprint $table) {
             $table->foreign('font_id')->references('id')->on('fonts')
+                ->onUpdate('cascade')
+                ->onDelete('set null');
+        });
+        
+        
+        Schema::table('comics', function(Blueprint $table) {
+            $table->foreign('created_by')->references('id')->on('users')
                 ->onUpdate('cascade')
                 ->onDelete('set null');
         });
@@ -37,6 +45,7 @@ class CreateComics extends Migration {
     public function down() {
         Schema::table('comics', function(Blueprint $table) {
             $table->dropForeign('comics_font_id_foreign');
+            $table->dropForeign('comics_created_by_foreign');
         });
         Schema::dropIfExists('comics');
     }
--- app/database/seeds/ComicsTableSeeder.php
+++ app/database/seeds/ComicsTableSeeder.php
@@ -11,7 +11,8 @@ class ComicsTableSeeder extends Seeder {
             'description' => 'Description Strumph',
             'authorApproval' => true,
             'cover' => '/uploads/0/ce7afa9115bb0b891963c632be0c4696',
-            'font_id' => 2
+            'font_id' => 2,
+            'created_by' => 2
         ]);
         Comic::create([
             'title' => 'Spiderman II',
@@ -19,7 +20,8 @@ class ComicsTableSeeder extends Seeder {
             'description' => 'Description SII',
             'authorApproval' => true,
             'cover' => '/uploads/0/d9480b185525aa7711522b34544fb0f6',
-            'font_id' => 3
+            'font_id' => 3,
+            'created_by' => 3
         ]);
     }
 }
