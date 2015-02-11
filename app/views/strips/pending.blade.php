20c3dc7c078629d7e1589b047efc11efe2b65830
amasline <amasline@172.22.247.110>
2015-02-11 17:57:37 +0100

    do css of pending.blade

--- app/views/strips/pending.blade.php
+++ app/views/strips/pending.blade.php
@@ -13,25 +13,30 @@
 @else
 <div class="row">
     @foreach ($strips as $strip)
-    <div class="col-md-4 padding-10">
+    <div class="col-sm-6 col-lg-4 padding-10">
         <div class="border padding-10">
-            <h3 class="one-line">{{ $strip->title }}</h3>
+            <div class="caption">
+                <h3 class="one-line">{{ ($strip->title)? $strip->title : $strip->comic." ".$strip->id }}</h3>
+            </div>
             {{ HTML::image($strip->path, 'strip', ['class' => 'img-responsive img-rounded', 'style' => 'overflow:hidden; width:250px; height:250px; display:block; margin:0 auto;']) }}
             <div class="caption">
-                <p>
-                    {{ Form::open(['method' => 'put', 'class'=>'form-horizontal', 'id' => 'stripForm'.$strip->id]); }}
-                    {{ Form::hidden('_method', 'put', ['id' => '_method']); }}
-                    {{ Form::hidden('id', $strip->id, ['id' => $strip->id]); }}
-
-
-                    {{ Form::submit(Lang::get('strips.pendingApprobation'),['class'=>'btn btn-lg btn-primary']); }}
-                    <span class="btn btn-lg btn-primary" id ="{{$strip->id}}" onclick="$('#_method').val('DELETE');
-                                $('#stripForm{{$strip->id}}').attr('action',
-                          '{{ URL::route('strips.destroy', [$strip->id]) }}'); $('#stripForm{{ $strip->id }}').submit();"> @lang('strips.pendingDelete') 
-                    </span>
-
-                    {{ Form::close(); }}
+                <p class="btn-group-justified">
+                    <a class='btn btn-sm btn-primary glyphicon glyphicon-edit' href=""> @lang('strips.pendingApprobation')</a>
+                    <a class='btn btn-sm btn-primary glyphicon glyphicon-edit' href="{{URL::route('strip.clean', ['id' => $strip->id])}}"> @lang('strips.pendingClean')</a>
+                    <a class='btn btn-sm btn-primary glyphicon glyphicon-edit' href="{{URL::route('strip.translate', ['id' => $strip->id])}}"> @lang('strips.pendingTranslate')</a>
                 </p>
+                    <!--{{ Form::open(['method' => 'put', 'class'=>'form-horizontal', 'id' => 'stripForm'.$strip->id]); }}
+                        {{ Form::hidden('_method', 'put', ['id' => '_method']); }}
+                        {{ Form::hidden('id', $strip->id, ['id' => $strip->id]); }}
+
+
+                        {{ Form::submit(Lang::get('strips.pendingApprobation'),['class'=>'btn btn-lg btn-primary']); }}
+                        <span class="btn btn-lg btn-primary" id ="{{$strip->id}}" onclick="$('#_method').val('DELETE');
+                                    $('#stripForm{{$strip->id}}').attr('action',
+                              '{{ URL::route('strips.destroy', [$strip->id]) }}'); $('#stripForm{{ $strip->id }}').submit();"> @lang('strips.pendingDelete') 
+                        </span>
+
+                        {{ Form::close(); }}-->
             </div>
         </div>
     </div>
