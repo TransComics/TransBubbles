f7ba0fcb8a3a7a9d5f40e138f24db6b173d9920a
Guillaume Roux <gu.roux@gmail.com>
2015-02-11 17:55:05 +0100

    Removing touches for the moment

--- app/models/Strips.php
+++ app/models/Strips.php
@@ -3,7 +3,7 @@
 class Strips extends Eloquent {
 
     protected $table = 'strips';
-    protected $touches = array('title','cleaning');
+    protected $touches = array('title');
     public $timestamps = true;
     protected $guarded = [ 'id', 'updated_at', 'created_at',
         'insertion_date', 'path', 'validated_at'];
