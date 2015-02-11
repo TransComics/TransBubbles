aa4cc928182788c252c79bae4aa0dd0ea5e26d30
amasline <amasline@172.22.247.110>
2015-02-11 18:02:40 +0100

    Update some fr translate in lang.fr.strips.php

--- app/lang/en/strips.php
+++ app/lang/en/strips.php
@@ -5,7 +5,6 @@ return array(
     'title' => 'Title',
     'author' => 'Author',
     'pageNumber' => 'Page Number',
-    'send' => 'Send',
     'uploadComplete' => 'Your image has been transferred',
     'uploadFailed' => 'Your image have not been transfered',
     'addTitle' => 'Add a Strip',
