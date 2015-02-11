9ee7de8e3b66b957a7436181c9a9dc21603d52b8
amasline <amasline@172.22.247.110>
2015-02-11 16:55:22 +0100

    do css of pending.blade

--- app/views/strips/pending.blade.php
+++ app/views/strips/pending.blade.php
@@ -13,24 +13,26 @@
 @else
 <div class="row">
     @foreach ($strips as $strip)
-    <div class="col-sm-6 col-md-4 thumbnail">
-        <h3>{{ $strip->title }}</h3>
-        {{ HTML::image($strip->path) }}
-        <div class="caption">
-            <p>
-                {{ Form::open(['method' => 'put', 'class'=>'form-horizontal', 'id' => 'stripForm'.$strip->id]); }}
-                {{ Form::hidden('_method', 'put', ['id' => '_method']); }}
-                {{ Form::hidden('id', $strip->id, ['id' => $strip->id]); }}
+    <div class="col-md-4 padding-10">
+        <div class="border padding-10">
+            <h3 class="one-line">{{ $strip->title }}</h3>
+            {{ HTML::image($strip->path, 'strip', ['class' => 'img-responsive img-rounded', 'style' => 'overflow:hidden; width:250px; height:250px; display:block; margin:0 auto;']) }}
+            <div class="caption">
+                <p>
+                    {{ Form::open(['method' => 'put', 'class'=>'form-horizontal', 'id' => 'stripForm'.$strip->id]); }}
+                    {{ Form::hidden('_method', 'put', ['id' => '_method']); }}
+                    {{ Form::hidden('id', $strip->id, ['id' => $strip->id]); }}
 
 
-                {{ Form::submit(Lang::get('strips.pendingApprobation'),['class'=>'btn btn-lg btn-primary']); }}
-                <span class="btn btn-lg btn-primary" id ="{{$strip->id}}" onclick="$('#_method').val('DELETE');
-                            $('#stripForm{{$strip->id}}').attr('action',
-                      '{{ URL::route('strips.destroy', [$strip->id]) }}'); $('#stripForm{{ $strip->id }}').submit();"> @lang('strips.pendingDelete') 
-                </span>
+                    {{ Form::submit(Lang::get('strips.pendingApprobation'),['class'=>'btn btn-lg btn-primary']); }}
+                    <span class="btn btn-lg btn-primary" id ="{{$strip->id}}" onclick="$('#_method').val('DELETE');
+                                $('#stripForm{{$strip->id}}').attr('action',
+                          '{{ URL::route('strips.destroy', [$strip->id]) }}'); $('#stripForm{{ $strip->id }}').submit();"> @lang('strips.pendingDelete') 
+                    </span>
 
-                {{ Form::close(); }}
-            </p>
+                    {{ Form::close(); }}
+                </p>
+            </div>
         </div>
     </div>
     @endforeach
