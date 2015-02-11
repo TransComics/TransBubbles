68086fc2ebdc9c51032ad5c3516d29cbb032c449
amasline <amasline@172.22.247.110>
2015-02-11 09:38:31 +0100

    add stripController

---
+++ app/controllers/StripController.php
@@ -0,0 +1,33 @@
+<?php
+
+class StripController extends BaseController {
+
+    /**
+     * import strip used by the controller.
+     *
+     * @return void
+     */
+    protected function import() {
+        return View::make('strip.import');
+    }
+
+    /**
+     * clean strip used by the controller.
+     *
+     * @return void
+     */
+    protected function clean() {
+        return View::make('strip.clean');
+    }
+
+    /**
+     * translate strip used by the controller.
+     *
+     * @return void
+     */
+    protected function translate() {
+        return View::make('strip.translate');
+    }
+
+}
+?>
