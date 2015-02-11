fb3d4d47a1b0b537419e103c58016d4e485b7c7c
Guillaume Roux <gu.roux@gmail.com>
2015-02-11 18:24:12 +0100

    Backing up with touches variable

--- app/controllers/StripsController.php
+++ app/controllers/StripsController.php
@@ -72,7 +72,7 @@ class StripsController extends BaseController {
 
         if ($v->passes()) {
             $strip->title = Input::get('title');
-            $strip->updated_at= new DateTime();
+            $strip->updated_at = new DateTime();
             $strip->save();
         } else {
             return Redirect::back()->with('message', Lang::get('strips.updateFailure'))
@@ -108,10 +108,10 @@ class StripsController extends BaseController {
         if ($strip == null) {
             return Redirect::back()->withInput()->withErrors($v);
         }
-        
-        $strip->validated_at= new DateTime();
+        $strip->updated_at = new DateTime();
+        $strip->validated_at = new DateTime();
         $strip->save();
-        
+
         return Redirect::back()->with('message', Lang::get('strips.approved'));
     }
 
