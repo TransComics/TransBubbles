e3d127fb4a1ec9c196c169a98409a749246d3daf
amasline <amasline@172.22.247.110>
2015-02-12 13:58:52 +0100

    add comic_id into seeder

--- app/database/seeds/StripsSeeder.php
+++ app/database/seeds/StripsSeeder.php
@@ -7,17 +7,20 @@ class StripsSeeder extends Seeder {
 
         Strips::create([
             'id' => 1,
+            'comic_id' => 2,
             'title' => 'Je suis projet',
             'path' => '/uploads/0/363b1c29fe79ecbd4a1777201ce11dee',
             'validated_at' => '2015-02-10 11:12:32',
         ]);
         Strips::create([
             'id' => 2,
+            'comic_id' => 1,
             'title' => 'Portable du père',
             'path' => '/uploads/0/cdb8a777ccf2c7c539cafe4e670c72e2',
         ]);
         Strips::create([
             'id' => 3,
+            'comic_id' => 2,
             'title' => 'Devop ?',
             'path' => '/uploads/0/58f19710798ee712be8efaeba131f768',
             'validated_at' => '2015-02-10 11:12:32',
