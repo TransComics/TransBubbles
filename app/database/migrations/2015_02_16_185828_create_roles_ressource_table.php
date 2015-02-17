3cccaf6f15b91699c3ae6f3220a27a89f2902c08
Guillaume Roux <gu.roux@gmail.com>
2015-02-17 13:45:51 +0100

    Adding user_id on roles_ressources table

--- app/database/migrations/2015_02_16_185828_create_roles_ressource_table.php
+++ app/database/migrations/2015_02_16_185828_create_roles_ressource_table.php
@@ -16,12 +16,16 @@ class CreateRolesRessourceTable extends Migration {
         Schema::create($this->table, function(Blueprint $table) {
             $table->increments('id');
             $table->integer('role_id')->unsigned();
+            $table->integer('user_id')->unsigned();
             $table->string('ressource')->nullable();
             $table->integer('ressource_id')->nullable();
         });
 
         Schema::table($this->table, function(Blueprint $table) {
             $table->foreign('role_id')->references('id')->on('roles')
+                    ->onUpdate('cascade')
+                    ->onDelete('restrict');
+            $table->foreign('user_id')->references('id')->on('users')
                     ->onUpdate('cascade')
                     ->onDelete('restrict');
         });
@@ -35,6 +39,7 @@ class CreateRolesRessourceTable extends Migration {
     public function down() {
         Schema::table($this->table, function(Blueprint $table) {
             $table->dropForeign($this->table . '_role_id_foreign');
+            $table->dropForeign($this->table . '_user_id_foreign');
         });
         Schema::dropIfExists($this->table);
     }
--- app/database/seeds/RolesRessourceTableSeeder.php
+++ app/database/seeds/RolesRessourceTableSeeder.php
@@ -4,13 +4,15 @@ class RolesRessourceTableSeeder extends Seeder {
 
     public function run() {
         RoleRessource::unguard();
-        
+
         RoleRessource::create([
             'role_id' => 1,
-            'ressource' => 'strip'
+            'ressource' => 'strip',
+            'user_id' => 1
         ]);
         RoleRessource::create([
-            'role_id' => '2'
+            'role_id' => '2',
+            'user_id' => 3
         ]);
     }
 
