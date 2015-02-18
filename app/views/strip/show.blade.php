402a79a5752f69c823d96ead908e5773bf1d996e
Geoffrey BERGERET <geoffrey.bergeret@gbergeret.org>
2015-02-18 23:49:42 +0100

    Add translate buttom in strip.show.

--- app/views/strip/show.blade.php
+++ app/views/strip/show.blade.php
@@ -42,15 +42,18 @@
 		<a href="{{URL::route('strip.index', array('comic_id' => $strips->comic_id))}}" class="btn btn-primary"><i class="glyphicon glyphicon-th"></i> View All</a>
 	</div>
 	<div class="btn-group" role="group">
-                {{ Form::open(['route' => ['strip.lang'], 'method' => 'post', 'id' => 'langStripForm', 'style' => 'display : inline;']) }}
-                {{ Form::select('lang_id', $available_languages, $lang_strip,['class'=>'btn btn-primary','data-width'=>'auto', 'onChange' => '$("#langStripForm").submit();'])}}
-                {{ Form::close() }}
-		<a href="{{URL::route('strip.vote', array('comic_id' => $strips->comic_id, 'strip_id' => $strips->id, 'lang' => 'fr'))}}" class="btn btn-primary">
-                        <i class="fa fa-comments-o fa-lg" style="padding-right: 5px;"></i> @lang('strip.other_translation')
-                </a>
-		<a href="{{URL::route('strip.vote', array('comic_id' => $strips->comic_id, 'strip_id' => $strips->id))}}" class="btn btn-primary">
-                        <i class="fa fa-globe fa-lg" style="padding-right: 5px;"></i>@lang('strip.languages')
-                </a>
+            {{ Form::open(['route' => ['strip.lang'], 'method' => 'post', 'id' => 'langStripForm', 'style' => 'display : inline;']) }}
+            {{ Form::select('lang_id', $available_languages, $lang_strip,['class'=>'btn btn-primary','data-width'=>'auto', 'onChange' => '$("#langStripForm").submit();'])}}
+            {{ Form::close() }}
+            <a href="{{URL::route('strip.vote', array('comic_id' => $strips->comic_id, 'strip_id' => $strips->id, 'lang' => 'fr'))}}" class="btn btn-primary">
+                    <i class="fa fa-comments-o fa-lg" style="padding-right: 5px;"></i> @lang('strip.other_translation')
+            </a>
+            <a href="{{URL::route('strip.vote', array('comic_id' => $strips->comic_id, 'strip_id' => $strips->id))}}" class="btn btn-primary">
+                    <i class="fa fa-globe fa-lg" style="padding-right: 5px;"></i>@lang('strip.languages')
+            </a>
+            <a href="{{URL::route('strip.translate', array('comic_id' => $strips->comic_id, 'strip_id' => $strips->id))}}" class="btn btn-primary">
+                    <i class="fa fa-comments-o fa-lg" style="padding-right: 5px;"></i> @lang('strip.translate')
+            </a>
 	</div>
 </div>
 @stop
