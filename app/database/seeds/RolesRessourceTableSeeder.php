99d37b98bccc6a8f6163308d6716b4da36859865
Guillaume Roux <gu.roux@gmail.com>
2015-02-19 09:08:30 +0100

    Uating RessourceTable Seeder

--- app/database/seeds/RolesRessourceTableSeeder.php
+++ app/database/seeds/RolesRessourceTableSeeder.php
@@ -4,14 +4,9 @@ class RolesRessourceTableSeeder extends Seeder {
 
     public function run() {
         RoleRessource::unguard();
-
+        
         RoleRessource::create([
-            'role_id' => 1,
-            'ressource' => 'strip',
-            'user_id' => 1
-        ]);
-        RoleRessource::create([
-            'role_id' => '2',
+            'role_id' => 2,
             'user_id' => 3
         ]);
     }
