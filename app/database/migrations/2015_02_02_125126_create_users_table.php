65c3935c6f7218f796365fe31fa79229bb47976b
azahou <ryushin75@gmail.com>
2015-02-23 13:55:12 +0100

    fix token size to 30

--- app/database/migrations/2015_02_02_125126_create_users_table.php
+++ app/database/migrations/2015_02_02_125126_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration {
 			$table->string('username', 128)->unique();
 			$table->string('password', 64);
 			$table->boolean('confirmed')->default(0);
-		    $table->string('confirmation_code')->nullable();
+		    $table->string('confirmation_code',30)->nullable();
 			$table->string('remember_token', 100)->nullable();
 			$table->timestamps();
 		});
