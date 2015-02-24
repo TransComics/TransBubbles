30e482ce6b35ad09fc6b72877d7c50c0dcbebbed
Guillaume Roux <gu.roux@gmail.com>
2015-02-24 12:04:55 +0100

    Creating Store and Remove userRole

--- app/controllers/RoleController.php
+++ app/controllers/RoleController.php
@@ -1,5 +1,7 @@
 <?php
 
+use Transcomics\RoleRessource\RessourceDefinition;
+
 class RoleController extends BaseController {
 
     public function __construct() {
@@ -33,14 +35,14 @@ class RoleController extends BaseController {
         $rules = array(
             'name' => 'required|unique:roles'
         );
-        
+
         $validator = Validator::make(Input::all(), $rules);
-        
+
         if ($validator->fails()) {
             Log::info('into store fail' . $validator->messages());
             return Redirect::back()->withErrors($validator)->withInput();
         } else {
-            
+
             $role = new Role();
             $role->name = Input::get('name');
             $role->C = Input::has('c') ? 1 : 0;
@@ -49,10 +51,81 @@ class RoleController extends BaseController {
             $role->M = Input::has('m') ? 1 : 0;
             $role->D = Input::has('d') ? 1 : 0;
             $role->save();
-            
+
             Log::info('saving role');
-            
+
             return Redirect::route('private..roles.index')->with('message', Lang::get('rote.created'));
+        }
+    }
+
+    /**
+     * Update or create user role
+     * @param type $role_id : The role ID impacted by the update
+     */
+    public function storeUserRole($role_id) {
+        $rules = array(
+            'name' => 'required',
+            'role' => 'required'
+        );
+
+        $validator = Validator::make(Input::all(), $rules);
+        if ($validator->fails()) {
+            Log::info('into store fail' . $validator->messages());
+            return Redirect::back()->withErrors($validator)->withInput();
+        }
+
+        $role = Role::find($role_id);
+        if ($role == NULL) {
+            Log::info('unknown role $role_id');
+            // TODO : Create lang
+            return Redirect::back()->withErrors(Lang::get('role.errorUpdatingRole'))->withInput();
+        }
+
+        $user = User::whereusername(Input::get('name'))->first();
+        if (empty($user)) {
+            Log::info('invalid user' . Input::get('name'));
+            //Todo : Create message
+            return Redirect::back()->withErrors(Lang::get('role.errorUpdatingRole'))->withInput();
+        }
+
+        $result = RoleRessource::addRight($role_id, RessourceDefinition::All, RessourceDefinition::All, $user->id);
+
+        if ($result) {
+            // Getting new table to update
+            $role_ressources = RoleRessource::whererole_id($role->id)->get();
+            return View::make('role.show', [
+                        'role' => $role,
+                        'role_ressources' => $role_ressources,
+            ]);
+        } else {
+            Log::info('unknown error when adding/updating userRole : addRight return false');
+            return Redirect::back()->withErrors(Lang::get('role.errorUpdatingRole'))->withInput();
+        }
+    }
+
+    public function removeUserRole($role_id, $roleRessource_id) {
+        
+        if (RoleRessource::removeRight($roleRessource_id)) {
+            $role = Role::find($role_id);
+            if ($role == null) {
+                return Redirect::route('home');
+            }
+
+           $role_ressources = RoleRessource::whererole_id($role->id)->get();
+           
+            if ($role_ressources == null) {
+                return Redirect::route('home');
+            }
+
+            return View::make('role.show', [
+                        'role' => $role,
+                        'role_ressources' => $role_ressources,
+            ]);
+            
+        } else {
+            Log::info('unknown error when removing userRole : removeRight returned false with role_id : $role_id');
+            // FIXME : 
+            return Redirect::back()->withErrors(Lang::get('role.errorDeleteRole'))->withInput();
         }
     }
 
@@ -67,9 +140,9 @@ class RoleController extends BaseController {
         if ($role == null) {
             return Redirect::route('home');
         }
-        
+
         return View::make('role.edit', [
-            'role' => $role
+                    'role' => $role
         ]);
     }
 
@@ -84,16 +157,16 @@ class RoleController extends BaseController {
         if ($role == null) {
             return Redirect::route('home');
         }
-        
+
         $role_ressources = RoleRessource::whererole_id($role->id)->get();
-        
+
         if ($role_ressources == null) {
             return Redirect::route('home');
         }
-        
+
         return View::make('role.show', [
-            'role' => $role,
-            'role_ressources' => $role_ressources
+                    'role' => $role,
+                    'role_ressources' => $role_ressources,
         ]);
     }
 
@@ -114,7 +187,7 @@ class RoleController extends BaseController {
         $role->M = Input::has('m') ? 1 : 0;
         $role->D = Input::has('d') ? 1 : 0;
         $role->save();
-        
+
         return Redirect::route('private..roles.index')->with('message', Lang::get('rote.updated'));
     }
 
@@ -132,12 +205,12 @@ class RoleController extends BaseController {
         $rolename = $role->name;
         if ($role->protected) {
             return Redirect::route('private..roles.index')->with('message', Lang::get('role.cannot_suppress', [
-                'rolename' => $rolename
+                                'rolename' => $rolename
             ]));
         }
         $role->delete();
         return Redirect::route('private..roles.index')->with('message', Lang::get('role.destroy', [
-            'rolename' => $rolename
+                            'rolename' => $rolename
         ]));
     }
 }
