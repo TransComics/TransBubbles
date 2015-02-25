824c4b311d4909cb5e001357ead746f1c315708d
Geoffrey BERGERET <geoffrey.bergeret@gbergeret.org>
2015-02-25 17:30:04 +0100

    Update validated_at to new

--- app/models/Strip.php
+++ app/models/Strip.php
@@ -95,20 +95,20 @@ class Strip extends Eloquent implements Moderable {
     }
     
     public function isCleanable() {
-        return $this->shapes()->whereNotNull('validated_at')->count() == 0;
+        return $this->shapes()->where('validated_state', ValidateEnum::VALIDATED)->count() == 0;
     }
 
     public function isImportable() {
         return $this->shapes()->where(function ($q) {
-                    $q->whereNotNull('validated_at')->orWhere('user_id', '=', Auth::id());
-                })->count() > 0 && $this->bubbles()->where('lang_id', $this->comic->lang_id)->whereNotNull('validated_at')->count() == 0;
+                    $q->where('validated_state', ValidateEnum::VALIDATED)->orWhere('user_id', '=', Auth::id());
+                })->count() > 0 && $this->bubbles()->where('lang_id', $this->comic->lang_id)->where('validated_state', ValidateEnum::VALIDATED)->count() == 0;
     }
 
     public function isTranslateable() {
         return $this->shapes()->where(function ($q) {
-                    $q->whereNotNull('validated_at')->orWhere('user_id', '=', Auth::id());
+                    $q->where('validated_state', ValidateEnum::VALIDATED)->orWhere('user_id', '=', Auth::id());
                 })->count() > 0 && $this->bubbles()->where(function ($q) {
-                    $q->whereNotNull('validated_at')->orWhere('user_id', '=', Auth::id());
+                    $q->where('validated_state', ValidateEnum::VALIDATED)->orWhere('user_id', '=', Auth::id());
                 })->count() > 0;
     }
 
