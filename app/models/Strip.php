77df36612853ea9d669a181c5d4d739214605c8e
Geoffrey BERGERET <geoffrey.bergeret@gbergeret.org>
2015-02-25 17:37:33 +0100

    BugFix getBestShapes()

--- app/models/Strip.php
+++ app/models/Strip.php
@@ -62,12 +62,13 @@ class Strip extends Eloquent implements Moderable {
         
     }
     
-    public function getBestShapes($user_id) {
+    public function getBestShapes($user_id = null) {
         return $this->shapes()
             ->where(function($query) use ($user_id) {
-                $query
-                    ->where('validated_state', ValidateEnum::VALIDATED)
-                    ->orWhere('user_id', '=', $user_id);
+                $query->where('validated_state', ValidateEnum::VALIDATED);
+                if (!is_null($user_id)) {
+                    $query->orWhere('user_id', '=', $user_id);
+                }
             })
             ->first();
     }
