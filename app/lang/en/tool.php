ee3e0c446b69d46931d725062206f6c3fbb0e91a
Guillaume Roux <gu.roux@gmail.com>
2015-02-26 17:15:33 +0100

    Tool language file

--- app/lang/en/tool.php
+++ app/lang/en/tool.php
@@ -1,5 +1,19 @@
 <?php
 
 return array(
-    
+    'hide' => 'Hide',
+    'select' => 'Select',
+    'trash' => 'Delete',
+    'color' => 'Color Selection',
+    'brushSize' => 'Brush Size',
+    'brush' => 'Brush',
+    'rectangle' => 'Rectangle',
+    'circle' => 'Circle',
+    'showAll' => 'Show All',
+    'selectAll' => 'Select All',
+    'zoomOut' => 'Zoom Out',
+    'zoomIn' => 'Zoom In',
+    'zoomReset' => 'Zoom Reset',
+    'undo' => 'Undo',
+    'redo' => 'Redo',    
 );
--- app/lang/fr/tool.php
+++ app/lang/fr/tool.php
@@ -1,5 +1,19 @@
 <?php
 
 return array(
-    
+    'hide' => 'Cacher',
+    'select' => 'Sélectionner',
+    'trash' => 'Supprimer',
+    'color' => 'Choix de la couleur',
+    'brushSize' => 'Taille Du Pinceau',
+    'brush' => 'Pinceau',
+    'rectangle' => 'Rectangle',
+    'circle' => 'Cercle',
+    'showAll' => 'Afficher Tout',
+    'selectAll' => 'Sélectionner Tout',
+    'zoomOut' => 'Zoom Arrière',
+    'zoomIn' => 'Zoom Avant',
+    'zoomReset' => 'Réinitialiser le Zoom',
+    'undo' => 'Annuler',
+    'redo' => 'Refaire', 
 );
